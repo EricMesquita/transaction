1493548a634d1f6cb6bdb7caf43897a298b40ed4
Eric Mesquita de Oliveira <eric.oliveira.mesquita@gmail.com>
2020-11-07 16:42:33 -0300

    removendo import não utilizado

--- src/main/java/com/transaction/transaction/controllers/AccountController.java
+++ src/main/java/com/transaction/transaction/controllers/AccountController.java
@@ -1,11 +1,9 @@
 package com.transaction.transaction.controllers;
 
 import javax.validation.Valid;
-import javax.websocket.server.PathParam;
 
 import lombok.AllArgsConstructor;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
